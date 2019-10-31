e9d557f03d593d961593e34e9cd5c893365f0661
JoSuh <52906059+JoSuh@users.noreply.github.com>
2019-10-31 12:12:05 -0400

    Update BinarySearch.java

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,197 +1,202 @@
-/****************************************************************************
-* Created by: Heejo Suh
- * Created on: Mar 2018
- * Created for: learning
- * 
- * This program generate a random list of 250 values and present them to the user. 
- * Then the list is sorted and presented to the user again. 
- * Finally you enter a number to be found. 
- * If the number is in the list, the position will be returned. 
- * If it is not in the list, the user will be informed of this.
- * 
- * 
- * 
- * Search a sorted array by repeatedly dividing the search interval in half.
- * Compare values and narrow into halves
- * Repeatedly check until the value is found or the interval is empty.
- *     
- *
- ****************************************************************************/
 
-
-
-//have to import the following:
-import java.util.Scanner;
-import java.util.Random;
-import java.math.*;
-
-import java.util.List;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 
-//****************************************************************************
-public class BinarySearch{
+/**
+ * 
+ * This class stores an ArrayList of Integers in sorted order using Collections.sort() to keep the ArrayList sorted
+ * Student Name: Jo Suh
+ *
+ */
+public class BinarySearch {
 
-	//------------------------
-	public static int binarySearch(List<Integer> inputArray , int inputNumber){
-	//Checks if input is in array and return answer
+	private static ArrayList<Integer> arrayList= new ArrayList<Integer>();
+
+	//===========================================================
+	public BinarySearch() {}
+	//===========================================================
+	public boolean canBeInt(String input, int ...range) {
+		// Asks a question and gets a number input from user
+		// if not the correct format, returns null
 		
-		int startCountingPoint = 0, lastCountingPoint = (inputArray.size() -1), middleValue;
-		//** to get length of listArray, you need to use '.size()'
+		try {
+			//check the string for whitespaces
+			if (input.isEmpty()) {
+				throw new InputMismatchException(); //invalid number
+			}
+			int userInt = Integer.valueOf(input);
+
+			//check if the number is in the correct range
+			if ( range.length> 0 && (userInt < range[0] || userInt > range[1])) {
+				throw new InputMismatchException(); //invalid number
+			}
+			return true;
+		} catch (InputMismatchException x) {
+			System.out.println("Please enter a valid integer");
+		} catch (NumberFormatException x) {
+			System.out.println("Please enter an integer");
+		}
+		return false;
+	}
+	//===========================================================
+	public boolean addItem(Scanner scanner) {
+
+		System.out.println("Please specify an Integer to add:");
+		String userInput =  scanner.nextLine(); //use nextLine to get the spaces
+		if (!canBeInt(userInput)) {
+			//invalid input
+			return false;
+		}
 		
+		arrayList.add(Integer.valueOf(userInput)); //add in value
+		Collections.sort(arrayList); // sort array
 		
-		while (startCountingPoint <= lastCountingPoint) {
-			//while range exists
-			//check halves for number 
-			
-			int middlePoint = (int) Math.round( (lastCountingPoint+startCountingPoint)/2 );
-			middleValue = inputArray.get(middlePoint);
-			
-			
-			//if input is at middleValue
-			if (inputNumber == middleValue) {
-				return middlePoint; //this breaks the 'while' and the 'binarySerach'
-				
-			} else if (inputNumber > middleValue) {
+		return true;
+	}
+	//===========================================================
+	public int searchItem(Scanner scanner) {
+		//searches for item in the array list and returns the index
+		// if item not found, returns -1
+
+		System.out.println("Please specify an Integer to search:");
+		String userInput =  scanner.nextLine(); //use nextLine to get the spaces
+		if (!canBeInt(userInput) || arrayList.size()==0 ) {
+			//invalid input
+			return -1;
+		}
+		int itemToCheck = Integer.valueOf(userInput);
+		// implements binary search
+		return binarySearch(itemToCheck, 0, arrayList.size()-1);
+	}
+	//===========================================================
+	public static int binarySearch(int inputNumber, int start, int end){
+		//Checks if input is in array and return answer
+		
+		if (end >=start) {
+			int midIndex = start + ((int) Math.round( (end-start)/2 ));
+			int midValue = arrayList.get(midIndex);
+	
+			if (midValue == inputNumber) {
+				//equal
+				return midIndex; 
+			}
+			if (midValue < inputNumber) {
 				//if input is greater than middleValue
 				//ignore left half
 				//all the values less will be useless
-				startCountingPoint = middlePoint + 1; //not including mid
-				
-			} else if (inputNumber < middleValue) {
+				return binarySearch(inputNumber, midIndex+1, end); 
+	
+			} else {
+				// if (inputNumber < midValue) {
 				//if input is less than middleValue
 				//ignore right half
 				//all the values greater will be useless
-				lastCountingPoint = middlePoint - 1; //not including mid
+				return binarySearch(inputNumber, start, midIndex-1); 
 			}
-		}//closing for while
-			
-		//if reach here, no values are in the list
-		//so return '-1'
-		return -1;
-    }//closing for binarySearch
-		
-	
-	
-	//------------------------
-	public static List<Integer> sortList(List<Integer> inputArray){
-			//Sorts the unsorted list and assigns the result to the sorted list
-
-			List<Integer> sortedList = new ArrayList<>();
-
-			Integer smallestVal = 0; //default is 0
-			
-			for(int count = 0 ; count<inputArray.size() ; ) {
-				//check all values of input array 
-				smallestVal = inputArray.get(0); //set as new smallest number
-				for(int eachValue: inputArray) {
-					//search for the smallest value in input array and restate it as such
-					//if the value is less than the smallest value, set it to new smallest value
-					smallestVal = (eachValue < smallestVal)? eachValue : smallestVal;
-				}
-				
-				sortedList.add(smallestVal); //add to sortedList
-				inputArray.remove(smallestVal); //remove the small value from input array
-			}
-			
-			return sortedList;
-			
-		
 		}
-	
-	
-	//------------------------
-	
-	public static void main(String args[]){
-	//uses the binarySearch function to search
+		return -1;
+		
+
+   }//closing for binarySearch
+			
+	//===========================================================
+	public String toString() {
+		String indexes="index:\t";
+		String list= "values:\t";
+		for (int i=0 ; i<arrayList.size() ; i++) {
+			indexes += i + "\t" ;
+			list += arrayList.get(i) + "\t";
+		}
+		return indexes + "\n" + list + "\n";
+	}
+
+	//===========================================================
+	public void displayMainMenu() {
+	// Outputs the main menu to the console (standard output)
+			/*
+			Please Enter:
+			1. Add Item
+			2. Search for Item
+			3. Display List
+			4. Exit
+			*/
+		
+		System.out.print("Please select one of the following:" + "\n"
+							+ "1. Add Item" + "\n"
+							+ "2. Search for Item" + "\n"
+							+ "3. Display List" + "\n"
+							+ "4. Exit"
+							);
+		
+	}
+	//===========================================================
+	public static void main(String[] args) {
+		// Runs the main program
 		
 		Scanner scanner = new Scanner(System.in);
-		String input = "", choice = "";
-		int inputNumber = 0, numberOfIntegersInList = 250;
 		
-		//create random list
-		List<Integer> randomList = new ArrayList<>(), sortedList = new ArrayList<>();
+		BinarySearch sample = new BinarySearch();
+		int userOption=0;
+		int menuMin = 1, menuMax = 4;
 		
-		
-		System.out.println("List:");
-		//add 250 random numbers
-		for ( int count=0; count<numberOfIntegersInList; count++ ) {  
-    		//add random number from 0 to 1000
-	    	int randNumber= new Random().nextInt(1000);
-	    	System.out.print(randNumber+" "); //show to user
-	    	randomList.add(randNumber);
-    	}
-		
-		
-		while (!choice.equals("E")) {
-			//not have exited program
+		//display the main menu console while the user has not exitted
+		program: while(true) {
 			
-			//get choice
-			while (!choice.equals("I") && !choice.equals("S")){
-				//see if user wants to input a value or search
-				System.out.println("\n\n\n-----------------------------------------------"
-						+ "\nI : insert value\nS : search for value\nE : exit");
+			//show options
+			sample.displayMainMenu();
 
-				choice = scanner.nextLine();
-				
-				if (! choice.equals("I") && !choice.equals("S")){
-					//error
-					System.out.println("Input a valid answer! Try again!");
+			//get user input
+			System.out.println("");
+			String userInput =  scanner.nextLine(); //use nextLine to get the spaces
+			if (!sample.canBeInt(userInput, menuMin, menuMax)) {
+				//invalid input: restart the menu
+				continue;
+			}
+			userOption = Integer.valueOf(userInput);
+			
+			
+			// if acceptable input, perform the corresponding actions
+			switch(userOption) {
+			
+			case 1:
+				// Add Item
+				if (!sample.addItem(scanner)) {
+					System.out.println("Failed to add item");
 				}
-			}//closing for get choice
-			
-			
-			//get number
-			while (input == ""){
-				//see if input is a number
-				System.out.println("\nInsert a number:");
+				break;
 				
-				try {
-					//check if response is an integer
-					input = scanner.nextLine();
-					inputNumber = Integer.parseInt(input);
-				}catch (IllegalArgumentException x) { 
-					System.out.println("Insert an integer!");
+				
+			case 2:
+				// Search for Item
+				int index = sample.searchItem(scanner);
+				System.out.print("Index of Integer is: ");
+				if (index==-1) {
+					System.out.println("Not Found");
+				}else {
+					System.out.println(index);
 				}
-			}//closing for get number
-			
-			
-			
-			//return
-			if (choice.equals("I")){
-				//insert value
-				randomList.add(inputNumber);
+				break;
+				
+				
+			case 3:
+				// Display List
+				System.out.println(sample.toString());
+				break;
+				
+				
+			case 4:
+				// Exit
+				System.out.println("Exiting...");
+				break program;
+				
 			}
 			
-			//sort
-			sortedList = sortList(randomList); //sorts list from least to greatest
-			
-			
-			System.out.print("\nSorted list:\n"+sortedList+"\n");
-			
-			if (choice.equals("S")){
-				//search value
-				int answer = binarySearch(sortedList, inputNumber);
-				
-				if (answer == -1) {
-					//not in the list
-		            System.out.println("\nNumber is not in the list!");
-				} else {
-					//in the list
+		}
+		scanner.close();
+	}
 
-		            System.out.println("\n"+inputNumber+" is at "+answer+" index!");
-				}
-			}
-			
+}
 
-			//need to reset to default
-			choice = ""; 
-			input = "";
-			inputNumber = 0;
-			
-		}//closing for while not have exitted the program
-		
-			
-    }//closing for main
-}//closing for BinarySearch
